30c8adaa8731cb79d353a5bb19243d519970c383
𝓥𝓮𝓲𝓵 <2054575@learn.vsb.bc.ca>
2026-09-16 15:00:20 -0700

    ConvertTime

--- ConvertTime.java
+++ ConvertTime.java
@@ -5,16 +5,18 @@ public class ConvertTime{
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the amount of seconds you want to convert to: hours, minutes, seconds.");
 		int inputS= scanner.nextInt();
-		int inputSBackup = inputS;
+int backupS= inputS;
+		//int answerH = inputS/3600;
+		//inputS-=(answerH*3600);		
+		//int answerM = inputS/60;
+		//inputS-=(answerM*60);
+		//int answerS = inputS;
+		
 		int answerH = inputS/3600;
-		inputS-=(answerH*3600);
-		
-		int answerM = inputS/60;
-		inputS-=(answerM*60);
-		int answerS = inputS;
-		
-		System.out.println(inputSBackup + " seconds = " + answerH + " hours, " + answerM + " minutes, " +"and " + answerS + " seconds.");
-		
+		int answerM = (inputS%3600)/60;
+		int answerS = (inputS%3600)%60;
+		//System.out.println(inputS + " seconds = " + answerH + " hours, " + answerM + " minutes, " +"and " + answerS + " seconds.");
+		System.out.printf("%dseconds = %d hours + %d minutes + %d seconds.",backupS,answerH,answerM,answerS);
 		
 		} 
 	}
